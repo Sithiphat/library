c428eca4f75a5f64adc0b343abe2bbe41cee56f5
Sithiphat <sithiphatk@gmail.com>
2023-01-04 13:33:00 +0700

    Change db to mysql2/promise and use pool

--- model/db.js
+++ model/db.js
@@ -1,16 +1,16 @@
-const config=require('config')
-const mysql= require('mysql2')
-const connection = mysql.createConnection({
-    host: config.get('mysql.host'),
-    user: config.get('mysql.user'),
-    password: config.get('mysql.password'),
-    database: config.get('mysql.database')
 
-});
+    const config=require('config')
+    const mysql= require('mysql2/promise')
 
-connection.connect(error =>{
-    if(error) throw error;
-    console.log("Successfully connected to the database.");
+    const pool =  mysql.createPool({
+        host: config.get('mysql.host'),
+        user: config.get('mysql.user'),
+        password: config.get('mysql.password'),
+        database: config.get('mysql.database')
+    
+    });
 
-});
-module.exports=connection;
+  
+
+
+module.exports=pool;
